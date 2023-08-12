227c4a7bcc74601ac5b4e1bda2ac989be9779fbb
yeram-lim <yeramyeye@gmail.com>
2023-08-12 16:12:25 +0900

    🍀[FEAT] login page

--- src/pages/account/login.jsx
+++ src/pages/account/login.jsx
@@ -2,13 +2,17 @@ import axios from "axios";
 import React, { useState } from "react";
 import { useForm } from "react-hook-form";
 import Input from "../../components/account/Input";
+import SquareButton from "@/components/button/SquareButton";
+import Image from "next/image";
+import { useRouter } from "next/router";
 
 const login = () => {
+  const router = useRouter();
   const {
     register,
     handleSubmit,
     // onSubmit,
-    formState: { isSubmitting, isSubmitted, errors },
+    formState: { isValid, isSubmitting, isSubmitted, errors },
   } = useForm();
   const [loginError, setLoginError] = useState(null);
 
@@ -34,40 +38,103 @@ const login = () => {
   return (
     <>
       <article>
-        <section></section>
+        <section>
+          <Image
+            src={`/login/account-pic.svg`}
+            width={250}
+            height={200}
+          ></Image>
+        </section>
         <section>
           <div>
             <div>로그인 하기</div>
-            <div>회원가입 하기</div>
+            <div onClick={() => router.push("/account/signup")}>
+              회원가입 하기
+            </div>
           </div>
           <div>
             <form onSubmit={handleSubmit(login)}>
-              <Input
-                formId={"id"}
-                placeholder={"아이디"}
-                errorMsg={loginError?.id ?? null}
-                register={register}
-              ></Input>
-              <Input
-                formId={"password"}
-                placeholder={"비밀번호"}
-                errorMsg={loginError?.password ?? null}
-                register={register}
-              ></Input>
-              {/* {errors.exampleRequired && <span>This field is required</span>} */}
-              {/* <input type="submit">로그인</input> */}
-              <button type="submit">로그인</button>
-              {/* onClick={() => onSubmit()} */}
+              <div>
+                <Input
+                  formId={"id"}
+                  placeholder={"아이디"}
+                  errorMsg={loginError?.id ?? null}
+                  register={register}
+                ></Input>
+                <Input
+                  formId={"password"}
+                  placeholder={"비밀번호"}
+                  errorMsg={loginError?.password ?? null}
+                  register={register}
+                ></Input>
+              </div>
+              <SquareButton
+                color={"mint"}
+                isDisabled={isValid ? false : true}
+                width={null}
+                height={null}
+                text={"로그인"}
+                buttonType={"submit"}
+              />
             </form>
           </div>
         </section>
         <section></section>
       </article>
       <style jsx>{`
-        form {
+        article {
           display: flex;
           flex-direction: column;
-          gap: 0.6875rem;
+          justify-content: flex-start;
+          align-items: center;
+          min-height: 100vh;
+          padding: 2rem;
+          > section {
+            width: 100%;
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+
+            > div {
+              width: 100%;
+            }
+          }
+          > section:first-child {
+            margin-bottom: 1.375rem;
+          }
+          > section:nth-child(2) {
+            width: 20.625rem;
+            > div:first-child {
+              display: flex;
+              justify-content: space-between;
+              align-items: center;
+              margin-bottom: 1.3125rem;
+              > div:first-child {
+                font-size: 1.5rem;
+                font-weight: 500;
+              }
+              > div:nth-child(2) {
+                font-size: 0.75rem;
+                font-weight: 400;
+                color: #089885;
+                text-decoration: underline;
+                cursor: pointer;
+              }
+            }
+          }
+        }
+
+        form {
+          width: 100%;
+          display: flex;
+          flex-direction: column;
+          gap: 3.75rem;
+          > div {
+            width: 100%;
+            display: flex;
+            flex-direction: column;
+            gap: 0.6875rem;
+          }
         }
       `}</style>
     </>
