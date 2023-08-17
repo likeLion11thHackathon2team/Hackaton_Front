8e1c1774803c0828de7a11dab4f8ef50839b6918
hyerim012 <phr01222@gmail.com>
2023-08-17 16:56:18 +0900

    findPage

--- src/pages/request/content.jsx
+++ src/pages/request/content.jsx
@@ -27,17 +27,28 @@ function Content() {
         });
     }
 
+    async function ConnectQuit() {
+        const response = await axios.delete('/requests/id',{
+            id: 1,
+        })
+        .then((response) => {
+            return response.data;
+        })
+        .catch((error) => {
+            return error;
+        });
+    }
+
     useEffect(() => {    
         navigator.geolocation.getCurrentPosition(function(pos) {
             var latitude = pos.coords.latitude;
             var longitude = pos.coords.longitude;
-            console.log("현재 위치는 :" + latitude + "," + longitude);
             setMentiLatitude(latitude);
             setMentiLongtitude(longitude);
-            // console.log("현재 위치는 :" + mentiLatitude+","+mentiLongtitude);
+            console.log("현재 위치는 :" + mentiLatitude + ","+ mentiLongtitude);
         });
     });
-
+    
     return (
         <div className="center">
             <div className="content-text">주문기기 관련 요청</div>
@@ -45,10 +56,10 @@ function Content() {
             <div className="content-text">어떤 부분에서 어려움을 겼으셨나요?</div>
             <textarea className="content-box"
                 placeholder="입력하기 ..."
-                onChange={()=>{}}
+                onChange={()=>{setContent(content)}}
             />
             <button onClick={()=>{Connect()}} className="btn-request">도움 요청하기</button>
-            <button onClick={()=>{}} className="btn-quit">요청 취소하기</button>
+            <button onClick={()=>{ConnectQuit()}} className="btn-quit">요청 취소하기</button>
         </div>
     );
 }
---
+++ src/pages/request/find.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Find() {
+    return (
+        <div>
+            <img className="find-word" src="/findimg/2KM 이내의 멘토 유저 찾는 중. . ..svg"/>
+            <img className="find-background" src="/findimg/findBackgroundImg.png"/>
+        </div>
+    );
+}
+export default Find;
