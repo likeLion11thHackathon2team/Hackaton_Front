3eceedc0040bddb462ef210124e2a82892bd451a
LimYeram <yeramyeye@gmail.com>
2023-08-19 02:00:25 +0900

    ➕[ADD] start user

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -3,8 +3,31 @@ import React from "react";
 const index = () => {
   return (
     <div>
-      <img className="first-img" src="/login/start.png"/>
-        <button onClick={()=>{window.location.href='/account/login'}} className="btn-login">로그인하기</button>
+      <img className="first-img" src="/login/start.png" />
+      <button
+        onClick={() => {
+          window.location.href = "/account/login";
+        }}
+        className="btn-login"
+      >
+        로그인하기
+      </button>
+      <button
+        onClick={() => {
+          window.location.href = "/request";
+        }}
+        className="btn-login"
+      >
+        비회원 멘티로 시작하기
+      </button>
+      <button
+        onClick={() => {
+          window.location.href = "/help_list";
+        }}
+        className="btn-login"
+      >
+        비회원 멘토로 시작하기
+      </button>
     </div>
   );
 };
