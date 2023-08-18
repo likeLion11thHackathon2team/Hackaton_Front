c401de61126ccd171c3d645ac003bb4565bf74d3
LimYeram <yeramyeye@gmail.com>
2023-08-19 02:52:57 +0900

    😺[CHORE] remove bottom bar in main

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import "../styles/global.css";
 import "../styles/main.css";
 import BottomBar from "@/components/bottomBar/BottomBar";
+import { useRouter } from "next/router";
 export default function App({ Component, pageProps }) {
+  const router = useRouter();
   return (
     <>
       <div className={`app`}>
         <Component {...pageProps} />
-        <BottomBar />
+        {!router.isReady || (router.asPath !== "/" && <BottomBar />)}
       </div>
       <style jsx>{`
         div.app {
