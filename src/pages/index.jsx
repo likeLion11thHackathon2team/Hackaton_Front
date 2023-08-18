624439c6d0e36b94e3c30a2d53d7a143b78196ab
hyerim012 <phr01222@gmail.com>
2023-08-18 23:24:30 +0900

    startPage

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,7 +1,12 @@
 import React from "react";
 
 const index = () => {
-  return <div>첫화면</div>;
+  return (
+    <div>
+      <img className="first-img" src="/login/start.png"/>
+        <button onClick={()=>{window.location.href='/account/login'}} className="btn-login">로그인하기</button>
+    </div>
+  );
 };
 
 export default index;
