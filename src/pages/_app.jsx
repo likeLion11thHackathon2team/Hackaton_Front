15941951250e5f43f94c3ab3fd3bdcc5b9974996
LimYeram <yeramyeye@gmail.com>
2023-08-19 02:48:59 +0900

    ➕[ADD]  bottom bar

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,7 +1,18 @@
 import React from "react";
 import "../styles/global.css";
 import "../styles/main.css";
-
+import BottomBar from "@/components/bottomBar/BottomBar";
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <div className={`app`}>
+        <Component {...pageProps} />
+        <BottomBar />
+      </div>
+      <style jsx>{`
+        div.app {
+        }
+      `}</style>
+    </>
+  );
 }
